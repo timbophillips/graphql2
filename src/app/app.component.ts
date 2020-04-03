053aa0a715f2f429128bb0e5a5b142382876e6cd
timbophillips <timothy.john.phillips@gmail.com>
2020-04-03 15:54:27 +0800

    tidy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,8 @@
 
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { NamesGQL } from '../generated/graphql';
 
 @Component({
   selector: 'app-root',
   template: '<app-names></app-names>',
 })
-export class AppComponent {
-
-  usersName$: Observable<string[]>;
-
-  constructor(userNameService: NamesGQL) {
-    this.usersName$ = userNameService.fetch({}).pipe(
-      map(result => result.data.names.map(user => user.name)),
-    );
-  }
-}
+export class AppComponent {}
